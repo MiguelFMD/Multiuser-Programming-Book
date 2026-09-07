946a9efb1fdc1f1e58ba31de28186196a6d01478
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2026-09-07 13:53:28 +0200

    Separated exercise spawn points

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function cargarMenu(rutaBase) {
                 <li><a href="${rutaBase}ejercicios/ejercicio-1.html"><i class="fa-solid fa-bookmark"></i> Exercise 1: The first multiplayer scene</a></li>
                 <li><a href="${rutaBase}ejercicios/ejercicio-2.html"><i class="fa-solid fa-bookmark"></i> Exercise 2: First multiplayer test</a></li>
                 <li><a href="${rutaBase}ejercicios/ejercicio-3.html"><i class="fa-solid fa-bookmark"></i> Exercise 3: Adding Network Behaviour</a></li>
+                <li><a href="${rutaBase}ejercicios/ejercicio-4.html"><i class="fa-solid fa-bookmark"></i> Exercise 4: Adding Spawn Points</a></li>
             </ul>
         </nav>
     `;
